2216f0949ee618ad1110ab365f6fa85c4daac3c9
Chhaileng Peng <chhailengpeng@gmail.com>
2020-08-11 13:59:53 +0900

    fix typo

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,7 +7,7 @@ export default class Educuation extends Component {
             <section id='education' className='section'>
                 <div className='container'>
                     <div className='section-title'>
-                        <h4 className='text-uppercase text-center'><img src={graduationIcon} alt='icon'/>Education</h4>
+                        <h4 className='text-uppercase text-center'><img src={graduationIcon} alt='icon' />Education</h4>
                     </div>
 
                     <div id='timeline-education'>
@@ -15,8 +15,8 @@ export default class Educuation extends Component {
                             <div className='timeline-dot'><h6>M</h6></div>
                             <div className='card timeline-content'>
                                 <div className='card-content'>
-                                    <h6 className='timeline-title'>Management System Information</h6>
-                                    <hr/>
+                                    <h6 className='timeline-title'>Management Information Systems</h6>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> Chungbuk National University</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> ~ Present</i></small></h6>
@@ -30,7 +30,7 @@ export default class Educuation extends Component {
                             <div className='card timeline-content'>
                                 <div className='card-content'>
                                     <h6 className='timeline-title'>Computer Science</h6>
-                                    <hr/>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> Royal University of Phnom Penh</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> 2014 - 2018</i></small></h6>
@@ -44,7 +44,7 @@ export default class Educuation extends Component {
                             <div className='card timeline-content'>
                                 <div className='card-content'>
                                     <h6 className='timeline-title'>IT Training</h6>
-                                    <hr/>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> Korea Software HRD Center</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> Mar 2017 - Feb 2018</i></small></h6>
@@ -58,7 +58,7 @@ export default class Educuation extends Component {
                             <div className='card timeline-content'>
                                 <div className='card-content'>
                                     <h6 className='timeline-title'>Digital Forensic</h6>
-                                    <hr/>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> Fujian, China</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> 2014</i></small></h6>
@@ -72,7 +72,7 @@ export default class Educuation extends Component {
                             <div className='card timeline-content'>
                                 <div className='card-content'>
                                     <h6 className='timeline-title'>High School</h6>
-                                    <hr/>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> Saang High School</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> 2014</i></small></h6>
@@ -86,7 +86,7 @@ export default class Educuation extends Component {
                             <div className='card timeline-content'>
                                 <div className='card-content'>
                                     <h6 className='timeline-title'>Other Traning</h6>
-                                    <hr/>
+                                    <hr />
                                     <div className='timeline-info'>
                                         <h6><small><i className='small material-icons'>place</i> [ Everywhere ]</small></h6>
                                         <h6><small><i><i className='small material-icons'>event</i> ~ Present</i></small></h6>
